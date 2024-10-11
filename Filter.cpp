cd3d2193568ab53d3b260fa1c3728802e64c1134
MertKadakal <mert.kadakal1629@gmail.com>
2024-10-11 17:36:44 +0300

    first commit: mean filter is done

---
+++ Filter.cpp
@@ -0,0 +1,59 @@
+#include "Filter.h"
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <math.h>
+#include "GrayscaleImage.h"
+#include <ostream>
+
+// Mean Filter
+void Filter::apply_mean_filter(GrayscaleImage& image, int kernelSize) {
+    // TODO: Your code goes here.
+    // 1. Copy the original image for reference.
+    // 2. For each pixel, calculate the mean value of its neighbors using a kernel.
+    // 3. Update each pixel with the computed mean.
+    int** data = image.get_data();
+
+    //copy the input image into another image
+    GrayscaleImage imageCopy(image);
+
+    //this for loop executes mean filter for each pixel
+    for (int i = 0; i < image.get_height(); i++) {
+        for (int j = 0; j < image.get_width(); j++) {
+            int total = 0;
+            int starting_height = i - (kernelSize-1)/2;
+            int starting_width = j - (kernelSize-1)/2;
+            for (int h = 0; h < kernelSize; h++) {
+                for (int w = 0; w < kernelSize; w++) {
+                    if (!(starting_height + h >= image.get_height() || starting_width + w >= image.get_width() || starting_height + h < 0 || starting_width + w < 0)) {
+                        total += data[starting_height + h][starting_width + w];
+                    }
+                }
+            }
+            int average = total/(kernelSize*kernelSize);
+            imageCopy.set_pixel(i, j, average);
+        }
+    }
+
+    //save the new image file as output.jpg
+    imageCopy.save_to_file("output.jpg");
+}
+
+// Gaussian Smoothing Filter
+void Filter::apply_gaussian_smoothing(GrayscaleImage& image, int kernelSize, double sigma) {
+    // TODO: Your code goes here.
+    // 1. Create a Gaussian kernel based on the given sigma value.
+    // 2. Normalize the kernel to ensure it sums to 1.
+    // 3. For each pixel, compute the weighted sum using the kernel.
+    // 4. Update the pixel values with the smoothed results.
+}
+
+// Unsharp Masking Filter
+void Filter::apply_unsharp_mask(GrayscaleImage& image, int kernelSize, double amount) {
+    // TODO: Your code goes here.
+    // 1. Blur the image using Gaussian smoothing, use the default sigma given in the header.
+    // 2. For each pixel, apply the unsharp mask formula: original + amount * (original - blurred).
+    // 3. Clip values to ensure they are within a valid range [0-255].
+}
